65e883fb6560da7c041a759b37addbedaa615209
Maxim Lee <mlee@key-g.com>
2017-08-08 21:15:28 +0300

    trying Smaato again

--- public/js/components/banner/banner.component.js
+++ public/js/components/banner/banner.component.js
@@ -4,6 +4,14 @@ app.component('banner', {
   },
   templateUrl: "templates/banner.html",
   controller: function($http, $scope, $rootScope, apiService) {
+    this.callBackForSmaato = function(status) {
+      if (status == "SUCCESS") {
+        console.log("callBack is being called with status : " + status);
+      } else if (status == "ERROR") {
+        console.log("callBack is being called with status : " + status);
+      }
+    };
     $scope.templatePath = '/templates/' + $rootScope.template + '/banner.html';
+    SomaJS.loadAd($ctrl.banner, this.callBackForSmaato);
   }
 });
--- public/js/components/poll/poll.component.js
+++ public/js/components/poll/poll.component.js
@@ -3,22 +3,22 @@
       poll: '<'
     },
     templateUrl: "templates/poll.html",
-    controller: function($http, $scope, $rootScope, apiService) {
+    controller: function ($http, $scope, $rootScope, apiService) {
       $scope.templatePath = '/templates/' + $rootScope.template + '/poll.html';
       var self = this;
-      apiService.hal_availability_check().then(function(data) {
+      apiService.hal_availability_check().then(function (data) {
         $scope.halMethod = data && data.method ? data.method : 'get'
       });
-      apiService.api_availability_check().then(function(data) {
+      apiService.api_availability_check().then(function (data) {
         $scope.apiMethod = data && data.method ? data.method : 'get'
       });
 
-      $scope.allowedRequest = function() {
+      $scope.allowedRequest = function () {
         return ($scope.halMethod == 'post' && $scope.apiMethod == 'post') ? 'post' : 'get'
       };
 
 
-      this._cookResult = function(selected) {
+      this._cookResult = function (selected) {
         var result = {};
         if (selected) {
           result = {
@@ -40,29 +40,29 @@
         return result
       };
 
-      this._getQuestionResults = function(question) {
+      this._getQuestionResults = function (question) {
         switch (question.question_type) {
-          case 'radio':
-            return [question.answer];
-            break;
-          case 'checkbox':
-            return question.answers.filter(function(answer) {
-              return answer.selected == true
-            });
-          case 'switch':
-            return question.answers.filter(function(answer) {
-              return answer.selected == true
-            })
+        case 'radio':
+          return [question.answer];
+          break;
+        case 'checkbox':
+          return question.answers.filter(function (answer) {
+            return answer.selected == true
+          });
+        case 'switch':
+          return question.answers.filter(function (answer) {
+            return answer.selected == true
+          })
         }
       };
 
-      this.send = function() {
+      this.send = function () {
         return apiService.sendPoll(this._cookResult(this.poll), $scope.allowedRequest())
       };
 
-      $scope.$on('sendPoll', function(event, callBack) {
+      $scope.$on('sendPoll', function (event, callBack) {
         self.send()
-          .then(function(data) {
+          .then(function (data) {
             if (data && data.data && (data.data.status !== 'error')) {
               callBack({
                 error: 'poll results sent'
@@ -73,7 +73,7 @@
               })
             }
           })
-          .catch(function(e) {
+          .catch(function (e) {
             callBack({
               error: e
             })
