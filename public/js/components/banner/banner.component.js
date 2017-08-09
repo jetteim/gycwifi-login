04a31f0c8586b72312133b8f7b29a763c3b490a3
Maxim Lee <mlee@key-g.com>
2017-08-09 15:13:27 +0300

    trying Smaato again

--- public/js/components/banner/banner.component.js
+++ public/js/components/banner/banner.component.js
@@ -5,10 +5,10 @@ app.component('banner', {
   templateUrl: "templates/banner.html",
   controller: function($http, $scope, $rootScope, apiService, reportService) {
     $scope.templatePath = '/templates/' + $rootScope.template + '/banner.html';
-    this.callBackForSmaato = function(status) {
-      reportService.sendstring(`SomaJS.loadAd() result: ${status}`)
-    };
     apiService.getSessionTargeting(this.session, 'get').then(function(data) {
+      this.callBackForSmaato = function(status) {
+        reportService.sendstring(`SomaJS.loadAd() result: ${status}`)
+      };
       SomaJS.loadAd(data, this.callBackForSmaato);
     });
 
