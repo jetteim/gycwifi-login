97236c483ef26d538525238645c744a764e06c88
Maxim Lee <mlee@key-g.com>
2017-08-17 23:56:53 +0300

    closing issue #5

--- public/js/components/main-view.component.js
+++ public/js/components/main-view.component.js
@@ -1,8 +1,10 @@
 app.component('mainView', {
   bindings: {},
   templateUrl: "templates/main-view.html",
-  controller: function($scope, $http, profileService, $element, $state, reportService, $stateParams, apiService, langService, $rootScope, pluginsService) {
+  controller: function($scope, $http, profileService, $element, $state, reportService, $stateParams, apiService, langService, $rootScope, pluginsService, $timeout) {
     // в index.jade ng-init = "session = '#{session}'", а в index.js в методе контроллера sessionCtrl отдали сессию в $rootScope.session
+
+    this.$timeout(this.pluginsService.initUi, 0);
 
     this.closeSideBar = function() {
       pluginsService.uiAction('sidebar_close')
