1ce54b3186fe5e378df26e6e0be421a510ecf2b1
Maxim Lee <mlee@key-g.com>
2017-08-18 00:53:58 +0300

    Update on-finish-render.directive.js

--- public/js/directives/on-finish-render.directive.js
+++ public/js/directives/on-finish-render.directive.js
@@ -1,13 +1,15 @@
-/* @ngInject */
-export default ($timeout) => {
-  return {
-    restrict: 'A',
-    link: function(scope, element, attr) {
-      if (scope.$last === true) {
-        $timeout(function() {
-          scope.$emit(attr.onFinishRender);
-        });
-      }
+app.directive('onFinishRender',['$timeout', '$parse', function ($timeout, $parse) {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attr) {
+            if (scope.$last === true) {
+                $timeout(function () {
+                    scope.$emit('ngRepeatFinished');
+                    if(!!attr.onFinishRender){
+                      $parse(attr.onFinishRender)(scope);
+                    }
+                });
+            }
+        }
     }
-  }
-}
+}])
