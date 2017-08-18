f3c400659ce48cf98091e689dd3411206be4f0e0
Maxim Lee <mlee@key-g.com>
2017-08-18 18:51:59 +0300

    close issue #5

---
+++ public/js/components/banner/banner.component.js
@@ -0,0 +1,18 @@
+app.component('banner', {
+  bindings: {
+    session: '<'
+  },
+  templateUrl: "templates/banner.html",
+  controller: function($http, $scope, $rootScope, apiService, reportService) {
+    $scope.templatePath = '/templates/' + $rootScope.template + '/banner.html';
+    var self = this;
+    apiService.getSessionTargeting(this.session, 'get').then(function(data) {
+      self.banner = data;
+      this.callBackForSmaato = function(status) {
+        reportService.sendstring(`SomaJS.loadAd() result: ${status}`)
+      };
+      SomaJS.loadAd(data, this.callBackForSmaato);
+    });
+
+  }
+});
